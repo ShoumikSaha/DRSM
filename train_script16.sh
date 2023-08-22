1b5c7339dfe6d6fd30edf7389b3f8b0d61452d81
Shoumik <34313576+ShoumikSaha@users.noreply.github.com>
2023-08-22 01:50:45 -0400

    Modified the scripts

--- train_script16.sh
+++ train_script16.sh
@@ -4,10 +4,10 @@ ROOT_DIR="$1"
 TRAIN_CSV="$2"
 VAL_CSV="$3"
 TEST_CSV="$4"
-EPOCHS="$5"
+BATCH_SIZE="$5"
 
 for (( i=0; i<16; i++ ))
 do
 	echo "$i"
-	python train_custom_malconv_by_ablation_from_csv.py --root_dir "$ROOT_DIR" --train_path "$TRAIN_CSV" --val_path "$VAL_CSV" --test_path "$TEST_CSV" --dir_path 'secml_malware/data/trained/smoothed_8' --ablation_idx $i --ablations 16 --epochs $EPOCHS --batch_size 16 >> output/train_16_output.txt
+	python train_custom_malconv_by_ablation_from_csv.py --root_dir "$ROOT_DIR" --train_path "$TRAIN_CSV" --val_path "$VAL_CSV" --test_path "$TEST_CSV" --dir_path 'secml_malware/data/trained/smoothed_8' --ablation_idx $i --ablations 16 --epochs 5 --batch_size $BATCH_SIZE >> output/train_16_output.txt
 done
--- train_script32.sh
+++ train_script32.sh
@@ -4,11 +4,11 @@ ROOT_DIR="$1"
 TRAIN_CSV="$2"
 VAL_CSV="$3"
 TEST_CSV="$4"
-EPOCHS="$5"
+BATCH_SIZE="$5"
 
 
 for (( i=0; i<32; i++ ))
 do
 	echo "$i"
-	python train_custom_malconv_by_ablation_from_csv.py --root_dir "$ROOT_DIR" --train_path "$TRAIN_CSV" --val_path "$VAL_CSV" --test_path "$TEST_CSV" --dir_path 'secml_malware/data/trained/smoothed_32' --ablation_idx $i --ablations 32 --epochs $EPOCHS --batch_size 16 >> output/train_32_output.txt
+	python train_custom_malconv_by_ablation_from_csv.py --root_dir "$ROOT_DIR" --train_path "$TRAIN_CSV" --val_path "$VAL_CSV" --test_path "$TEST_CSV" --dir_path 'secml_malware/data/trained/smoothed_32' --ablation_idx $i --ablations 32 --epochs 5 --batch_size $BATCH_SIZE >> output/train_32_output.txt
 done
