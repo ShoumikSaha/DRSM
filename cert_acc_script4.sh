33711a364320583fe31ad315d7274ba5052a6212
Shoumik <34313576+ShoumikSaha@users.noreply.github.com>
2023-09-28 18:51:00 -0400

    Adding certified acc script

--- cert_acc_script4.sh
+++ cert_acc_script4.sh
@@ -5,4 +5,4 @@ TRAIN_CSV="$2"
 VAL_CSV="$3"
 TEST_CSV="$4"
 
-python evaluate_custom_malconv_cert_acc.py --root_dir "$ROOT_DIR" --train_path "$TRAIN_CSV" --val_path "$VAL_CSV" --test_path "$TEST_CSV" --dir_path 'secml_malware/data/trained/smoothed_4' --ablations 4 --batch_size 64 --perturb_size 10000,20000,50000,100000,150000,200000,250000,300000,350000,400000,450000,500000 >> output/cert_4_output.txt
+python evaluate_custom_malconv_cert_acc.py --root_dir "$ROOT_DIR" --train_path "$TRAIN_CSV" --val_path "$VAL_CSV" --test_path "$TEST_CSV" --dir_path 'secml_malware/data/trained/smoothed_4' --ablations 4 --batch_size 128 --perturb_size 200000,600000 >> output/cert_4_output2.txt
